a790e99bb00f17c664cd5127845d3381a79fb327
hayleyhadges <hhadges@macalester.edu>
2019-10-28 11:50:03 -0500

    Finished HW3

---
+++ src/comp127/graphingCalculator/ParametricFunction.java
@@ -0,0 +1,5 @@
+package comp127.graphingCalculator;
+
+public interface ParametricFunction {
+    double evaluate(double x, double n);
+}
---
+++ src/comp127/graphingCalculator/SimpleFunction.java
@@ -0,0 +1,5 @@
+package comp127.graphingCalculator;
+
+public interface SimpleFunction {
+    double evaluate(double x);
+}
